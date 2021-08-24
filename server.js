784f48006904af18fe754f42b44f3602138b5c65
Joelle DesChatelets <jdeschat@gmail.com>
2021-08-24 11:04:02 -0400

    added query parameters for animal

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const app = express();
 const { animals } = require('./data/animals');
 
 app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
+    let results = animals;
+    console.log(req.query)
+    res.json(results);
 });
 
 app.listen(3001, () => {
